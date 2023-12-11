5150c1a7404cdda5ffae89ca235f9c59a46fce97
Lukman Sanjaya <lukman.rocks@live.com>
2023-12-11 21:14:05 +0700

    add UnaryInterceptor and dummy api key

--- learn-grpc-03/server/main.go
+++ learn-grpc-03/server/main.go
@@ -5,11 +5,23 @@ import (
 	"fmt"
 	pb "github.com/lukmansjy/grpc-basic/learn-grpc-03/user"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/metadata"
+	"google.golang.org/grpc/status"
+	"log"
 	"net"
 )
 
 type server struct {
 	pb.UnimplementedUserServer
+}
+
+func isValidAppKey(appKey []string) bool {
+	if len(appKey) < 1 {
+		return false
+	}
+
+	return appKey[0] == "1234545"
 }
 
 func (s *server) UnaryGetUser(ctx context.Context, request *pb.UserRequest) (*pb.UserResponse, error) {
@@ -21,13 +33,31 @@ func (s *server) UnaryGetUser(ctx context.Context, request *pb.UserRequest) (*pb
 	}, nil
 }
 
+func unaryInterceptorImpl(ctx context.Context, req any, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (resp any, err error) {
+	fmt.Printf("Incomming request: %s \n", info.FullMethod)
+	md, ok := metadata.FromIncomingContext(ctx)
+	if !ok {
+		return nil, status.Error(codes.InvalidArgument, "Invalid Argument")
+	}
+
+	if !isValidAppKey(md["app_key"]) {
+		return nil, status.Error(codes.Unauthenticated, "Unauthenticated")
+	}
+
+	m, err := handler(ctx, req)
+	if err != nil {
+		log.Fatal("error in grpc service", err)
+	}
+	return m, err
+}
+
 func main() {
 	lis, err := net.Listen("tcp", ":9000")
 	if err != nil {
 		fmt.Println(err.Error())
 	}
 
-	s := grpc.NewServer()
+	s := grpc.NewServer(grpc.UnaryInterceptor(unaryInterceptorImpl))
 	pb.RegisterUserServer(s, &server{})
 	if err := s.Serve(lis); err != nil {
 		fmt.Println(err.Error())
